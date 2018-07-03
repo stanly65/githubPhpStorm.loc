cff850887e2cd233fcefe98d0fa71c0dd9722cc7
Yoanski <yoancho2@gmail.com>
2018-07-03 15:55:25 +0300

    Why function from Students.php not work ?

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+
 echo 'Hello PHP';
 echo "\n\r";
 $str =  "Hi Yoan";
 printf($str . " from Stanly");
+$arr = ['Stanimir', 'Yoan', 'Gesha', "Pepi"];
+echo "\n\r";
+
+print "Best Softuni students is : " . "\r\n";
+print_r( $arr);
+
+require_once ('Students.php');
+$a = getSortedList($arr);
+print_r($a);
